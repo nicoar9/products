57a5be4d99f3969ae1c38e488dfdf82c7d4442cb
Nicholas Abad <nicoar9@gmail.com>
2021-03-05 16:48:30 -0500

    :sparkles:Feature: Adds method for uploading photos

--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'package:http_parser/http_parser.dart';
 import 'package:http/http.dart' as http;
+import 'package:mime_type/mime_type.dart';
 import 'package:products_app/models/product_model.dart';
 
 class ProductsService {
@@ -49,5 +51,31 @@ class ProductsService {
     final url = '$_url/products/$id.json';
 
     final resp = await http.delete(url);
+  }
+
+  Future<String> uploadImage(File image) async {
+    final url = Uri.parse(
+        'https://api.cloudinary.com/v1_1/dyrpxoxni/image/upload?upload_preset=kuyiiuxj');
+    final mimeType = mime(image.path).split('/');
+    final imageUploadRequest = http.MultipartRequest('POST', url);
+
+    final file = await http.MultipartFile.fromPath('file', image.path,
+        contentType: MediaType(mimeType[0], mimeType[1]));
+
+    imageUploadRequest.files.add(file);
+
+    final streamResponse = await imageUploadRequest.send();
+
+    final resp = await http.Response.fromStream(streamResponse);
+
+    if (resp.statusCode != 200 && resp.statusCode != 201) {
+      print('something went wrong');
+      print(resp.body);
+      return null;
+    }
+
+    final respData = json.decode(resp.body);
+    print(respData);
+    return respData['secure_url'];
   }
 }
